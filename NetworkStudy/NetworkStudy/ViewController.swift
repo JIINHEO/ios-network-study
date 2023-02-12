305a952c85a5e6bb1cb0548c2bb0b90418bc71f7
JIINHEO <gjwldls123@gmail.com>
2023-02-12 18:34:14 +0900

    initail commit

---
+++ NetworkStudy/NetworkStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkStudy
+//
+//  Created by jiinheo on 2023/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
