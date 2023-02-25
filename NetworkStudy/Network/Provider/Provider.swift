01fc276426e398810cd6e871a31e3d1ac31f9b42
JIINHEO <gjwldls123@gmail.com>
2023-02-25 19:49:10 +0900

    Feat: Implement Provider class

---
+++ NetworkStudy/Network/Provider/Provider.swift
@@ -0,0 +1,85 @@
+//
+//  Provider.swift
+//  NetworkStudy
+//
+//  Created by jiinheo on 2023/02/25.
+//
+
+import Foundation
+
+protocol Provider {
+    /// 특정 responsable이 존재하는 request
+    func request<R: Decodable, E: RequestResponsable>(with endpoint: E, completion: @escaping (Result<R, Error>) -> Void) where E.Response == R
+
+    /// data를 얻는 request
+    func request(_ url: URL, completion: @escaping (Result<Data, Error>) -> ())
+}
+
+class ProviderImpl: Provider {
+    func request<R, E>(with endpoint: E, completion: @escaping (Result<R, Error>) -> Void) where R : Decodable, R == E.Response, E : RequestResponsable {
+        do {
+            let urlRequest = try endpoint.getUrlReauest()
+
+            session.dataTask(with: urlRequest) { [weak self] data, response, error in
+                self?.checkError(with: data, response, error, completion: { result in
+                    guard let self = self else {return}
+                    
+                    switch result {
+                    case .success(let data):
+                        completion(self.decode(data: data))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                })
+            }.resume()
+        } catch {
+            completion(.failure(NetworkError.urlRequest(error)))
+        }
+    }
+    
+    func request(_ url: URL, completion: @escaping (Result<Data, Error>) -> ()) {
+        session.dataTask(with: url) { [weak self] data, response, error in
+            self?.checkError(with: data, response, error, completion: { result in
+                completion(result)
+            })
+        }.resume()
+    }
+    
+    let session: URLSession
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    private func checkError(with data: Data?, _ response: URLResponse?, _ error: Error?, completion: @escaping (Result<Data, Error>) -> ()) {
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+        
+        guard let response = response as? HTTPURLResponse else {
+            completion(.failure(NetworkError.unknownError))
+            return
+        }
+        
+        guard (200...299).contains(response.statusCode) else {
+            completion(.failure(NetworkError.invalidHttpStatusCode(response.statusCode)))
+            return
+        }
+        
+        guard let data = data else {
+            completion(.failure(NetworkError.emptyData))
+            return
+        }
+        
+        completion(.success(data))
+    }
+    
+    private func decode<T: Decodable>(data: Data) -> Result<T, Error> {
+        do {
+            let decoded = try JSONDecoder().decode(T.self, from: data)
+            return .success(decoded)
+        } catch {
+            return .failure(NetworkError.emptyData)
+        }
+    }
+}
